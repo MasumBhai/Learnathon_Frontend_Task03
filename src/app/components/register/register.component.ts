727e7dbc8f23a541ad5266c857b49435e1f62644
MasumBhai <abdullahmasum6035@gmail.com>
2022-06-04 18:28:30 +0600

    Beautiful template done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
+import {ConfirmedValidator} from './confirmed.validator';
 
 @Component({
   selector: 'app-register',
@@ -9,24 +10,83 @@ import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
   Roles: any = ['Admin', 'Author', 'Reader'];
-  email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+  hide_pass = true;
+  hide_conf_pass = true;
   model: any | NgbDateStruct;
-  panelOpenState = false;
+  panelOpenState = true;
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
+  registerForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: ConfirmedValidator('password', 'confirm_password')
+      }
+    );
   }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.registerForm.value, null, 2)); //todo: change here
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
+  get f():{ [key: string]: AbstractControl }{
+    return this.registerForm.controls;
+  }
+
+  get userName() {
+    return this.registerForm.get('username')
+  }
+
+  // getErrorMessage() {
+  //   if (userName().hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+  //
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+
 
 }
