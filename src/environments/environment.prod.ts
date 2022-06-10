069871b025b886bbedbe9ca8972b32fa3c0bad56
MasumBhai <abdullahmasum6035@gmail.com>
2022-06-11 02:46:03 +0600

    task03 at last done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://three60.learnathon.net/api1/api/register'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://three60.learnathon.net/api1/api/register'
 };
 
