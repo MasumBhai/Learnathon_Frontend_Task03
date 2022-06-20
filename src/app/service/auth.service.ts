dfec34626fbe847b321119084d1347d037f6d6d5
MasumBhai <abdullahmasum6035@gmail.com>
2022-06-20 10:35:34 +0600

    api endpoint added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable, of} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {JwtHelperService} from "@auth0/angular-jwt";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -17,13 +18,15 @@ the user information which makes our application authentication process inconsis
 load the user information in the 'AuthService' constructor.
 */
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.loadUserInfo();
   }
 
   userLogin(login: any): Observable<boolean> {
     if (login && login.log_user && login.log_password) {
-      const sampleJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRlc3QiLCJzdWIiOjIsImlhdCI6MTYwNDMwOTc0OSwiZXhwIjoxNjA0MzA5ODA5fQ.jHez9kegJ7GT1AO5A2fQp6Dg9A6PBmeiDW1YPaCQoYs";
+      // These lines are for mocked JWT token practice
+      const sampleJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkJyYWlueV9Gb29sIiwicGFzc3dvcmQiOiJCb2xib19OYSJ9.eGR_PzuAwLoJGR8sEroPHDAcqQzLL8vM21MRKrDYygs";
+      const refreshToken = "dummy";
 
       return of(sampleJWT).pipe(
         map((token) => {
@@ -33,9 +36,23 @@ load the user information in the 'AuthService' constructor.
           localStorage.setItem("access_token", token); // For SPA, common approach to store, the token is in browser local storage.
           const decodedUser = this.jwtHelper.decodeToken(token);
           this.userInfo.next(decodedUser);
-          // alert("ok")
+          // console.log(decodedUser)
           return true;
         }));
+
+
+      // // to use the API endpoint for user login in the AuthService
+      // return this.http.post("http://localhost:3000/auth/login", login).pipe( //todo: change url of the token api server
+      //   map((data: any) => {
+      //     if (!data) {
+      //       return false;
+      //     }
+      //     localStorage.setItem('access_token', data.access_token); // this method is vulnerable to XSS attack
+      //     const decodedUser = this.jwtHelper.decodeToken(data.access_token);
+      //     this.userInfo.next(decodedUser);
+      //     return true;
+      //   })
+      // );
     }
     // console.log(JSON.stringify(this.log_in_form.value,null,2))
     return of(false); // the 'of' operator to make observable type because when we change the logic to API code that needs to be rewritten will be very less
